d7f838d47c9537bcc1f5c1cc85377833f48163b5
mancuoj <mancuoj@gmail.com>
2025-06-05 00:08:31 +0800

    feat: prettier and eslint

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html'
+  templateUrl: './app.html',
 })
-export class App {
-  protected title = 'angular-tmpl';
+export class AppComponent {
+  protected title = 'angular-tmpl'
 }
