aa4360a5872920bb07544b3750abfccda4bfe3cb
mancuoj <mancuoj@gmail.com>
2025-06-07 00:27:28 +0800

    chore: update

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,19 @@ import { ThemeToggleComponent } from './components/theme-toggle'
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ThemeToggleComponent],
-  templateUrl: './app.html',
+  template: `
+    <div class="relative font-sans antialiased">
+      <div class="flex flex-col items-center justify-center h-svh">
+        <div class="flex gap-5 items-center">
+          <a class="btn btn-primary" href="https://github.com/mancuoj-collective/angular-tmpl">
+            <span class="iconify carbon--logo-github size-4"></span>
+            GitHub
+          </a>
+          <app-theme-toggle />
+        </div>
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+  `,
 })
 export class AppComponent {}
--- src/app/components/theme-toggle.ts
+++ src/app/components/theme-toggle.ts
@@ -1,40 +1,30 @@
-import { Component, effect, signal } from '@angular/core'
+import { Component, computed, effect, signal } from '@angular/core'
 
-const THEME_KEY = 'angular-tmpl-theme'
-
-function getInitialTheme() {
-  if (typeof window === 'undefined') return 'light'
-  return localStorage.getItem(THEME_KEY) || 'light'
-}
+const STORAGE_KEY = 'angular-tmpl-theme'
 
 @Component({
   selector: 'app-theme-toggle',
   standalone: true,
   template: `
     <label class="swap swap-rotate">
-      <input
-        type="checkbox"
-        class="theme-controller"
-        [checked]="theme() === 'dark'"
-        (change)="toggle()"
-      />
+      <input type="checkbox" class="theme-controller" [checked]="isDark()" (change)="toggle()" />
       <span class="swap-on iconify carbon--moon size-5"></span>
       <span class="swap-off iconify carbon--sun size-5"></span>
     </label>
   `,
 })
 export class ThemeToggleComponent {
-  theme = signal(getInitialTheme())
+  theme = signal(localStorage.getItem(STORAGE_KEY) || 'light')
+  isDark = computed(() => this.theme() === 'dark')
 
   constructor() {
     effect(() => {
-      const t = this.theme()
-      document.documentElement.setAttribute('data-theme', t)
-      localStorage.setItem(THEME_KEY, t)
+      document.documentElement.setAttribute('data-theme', this.theme())
+      localStorage.setItem(STORAGE_KEY, this.theme())
     })
   }
 
   toggle() {
-    this.theme.set(this.theme() === 'dark' ? 'light' : 'dark')
+    this.theme.set(this.isDark() ? 'light' : 'dark')
   }
 }
