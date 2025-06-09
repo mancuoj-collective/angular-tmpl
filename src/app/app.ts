2493390c57fe2d892b9bef6e12e676325d3dd723
mancuoj <mancuoj@gmail.com>
2025-06-09 18:38:22 +0800

    chore: update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { ThemeToggleComponent } from './components/theme-toggle'
+import { ThemeToggleComponent } from '@/components/theme-toggle'
+import { GithubLinkComponent } from '@/components/github-link'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ThemeToggleComponent],
+  imports: [RouterOutlet, ThemeToggleComponent, GithubLinkComponent],
   template: `
     <div class="relative font-sans antialiased">
       <div class="flex flex-col items-center justify-center h-svh">
         <div class="flex gap-5 items-center">
-          <a class="btn btn-primary" href="https://github.com/mancuoj-collective/angular-tmpl">
-            <span class="iconify carbon--logo-github size-4"></span>
-            GitHub
-          </a>
+          <app-github-link />
           <app-theme-toggle />
         </div>
         <router-outlet></router-outlet>
---
+++ src/components/github-link.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-github-link',
+  template: `
+    <a class="btn btn-primary" href="https://github.com/mancuoj-collective/angular-tmpl">
+      <span class="iconify carbon--logo-github size-4"></span>
+      GitHub
+    </a>
+  `,
+})
+export class GithubLinkComponent {}
