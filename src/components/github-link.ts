e5803b24c42d15b5b4c6c6af108b87859b0d43d5
mancuoj <mancuoj@gmail.com>
2025-06-09 18:39:19 +0800

    chore: update

--- src/components/github-link.ts
+++ src/components/github-link.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-github-link',
+  standalone: true,
   template: `
     <a class="btn btn-primary" href="https://github.com/mancuoj-collective/angular-tmpl">
       <span class="iconify carbon--logo-github size-4"></span>
